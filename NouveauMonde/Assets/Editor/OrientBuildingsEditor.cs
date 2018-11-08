9375a6afcc503849b75b25e4ee841982ec3c1827
grusscharles <grusscharles@gmail.com>
2018-11-08 18:58:44 +0100

    Ajout Orientation Buildings + Skybox + Earth Mat

---
+++ NouveauMonde/Assets/Editor/OrientBuildingsEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Orient))]
+public class OrientBuildingsEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Orient orient = (Orient)target;
+
+        if (GUILayout.Button("Orient Buildings"))
+        {
+            orient.OrientBuildings();
+        }
+    }
+}
---
+++ NouveauMonde/Assets/_Scripts/Orient.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orient : MonoBehaviour {
+
+    public void OrientBuildings()
+    {
+        GameObject[] buildingsArray = GameObject.FindGameObjectsWithTag("building");
+        foreach(GameObject building in buildingsArray)
+        {
+            Vector3 normal = building.transform.position.normalized;
+            building.transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
+        }
+        
+    }
+
+}
