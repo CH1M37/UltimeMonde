6f2ad2b40aef71614807f4fd1d63d1f804395c26
CH1M37 <CH1M37>
2018-11-12 21:05:47 +0100

    Ajout et test material-textures mouvantes UI

---
+++ NouveauMonde/Assets/_Scripts/UI/MovingTex.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingTex : MonoBehaviour {
+
+    public float ScrollX = 0.5f;
+    public float ScrollY = 0.5f;
+
+	void Update () {
+
+        float OffsetX = Time.time * ScrollX;
+        float OffsetY = Time.time * ScrollY;
+
+        GetComponent<Image>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+
+        // GetComponent<RawImage>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+
+        // GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+
+	}
+}
