e571f3a1de132f9dd993b6cfebe87efecdfc4487
CH1M37 <CH1M37>
2018-11-17 16:55:33 +0100

    Ajout fctn PAUSE, MenuPAuse, polish UI

--- NouveauMonde/Assets/_Scripts/UI/UIEndGame.cs
+++ NouveauMonde/Assets/_Scripts/UI/UIEndGame.cs
@@ -6,6 +6,7 @@ public class UIEndGame : MonoBehaviour {
 
     public GameObject GameOverScreen;
     public GameObject WinScreen;
+    public GameObject PauseMenu;
 
     public void GameOver()
     {
@@ -18,15 +19,35 @@ public class UIEndGame : MonoBehaviour {
 
     public void Win()
     {
-        if (GameOverScreen.activeSelf == false)
+        if (WinScreen.activeSelf == false)
         {
             WinScreen.SetActive(true);
             Time.timeScale = 0;
         }
     }
 
+    public void PauseGame()
+    {
+        if (Time.timeScale != 0 && GameOverScreen.activeSelf == false && WinScreen.activeSelf == false)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0 && GameOverScreen.activeSelf == false && WinScreen.activeSelf == false)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
     void Update()
     {
+
+        if (Input.GetKeyDown("escape"))
+        {
+            PauseGame();
+        }
+
         /*
         //Game Over
         if (Input.GetKeyDown(KeyCode.C))
