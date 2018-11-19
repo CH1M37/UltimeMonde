e1d133090746820272733483c6057b761c5cc8a8
CH1M37 <CH1M37>
2018-11-19 21:42:47 +0100

    Tweaks sur le mouvement du ragdoll

--- NouveauMonde/Assets/_Scripts/PlayerController/RagController.cs
+++ NouveauMonde/Assets/_Scripts/PlayerController/RagController.cs
@@ -5,31 +5,33 @@ using UnityEngine;
 public class RagController : MonoBehaviour {
 
     private Vector3 moveDirection;
+    
 
     public GameObject CubeController;
     public GameObject Animation;
+    //public GameObject BodyToRotate;
 
-    public float speed = 0.5f;
+    public float speed = 0.1f;
+    public float rotationSpeed = 20f;
 
 	void Update () {
-
+        
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
         moveDirection = (v * transform.forward + h * transform.right).normalized;
 
         CubeController.transform.Translate(moveDirection * speed);
-
-        // Déclancher animation pieds si ZQSD
-        //if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+        CubeController.transform.RotateAround(CubeController.transform.position, CubeController.transform.up, Time.deltaTime * Input.GetAxisRaw("Camera") * rotationSpeed);
+        
+        // Déclancher animations pieds si MOVE
         if (moveDirection.magnitude > 0)
-        {   
+        {
             Animation.GetComponent<Animator>().enabled = true;
         }
         else
         {
             Animation.GetComponent<Animator>().enabled = false;
         }
-
     }
 }
