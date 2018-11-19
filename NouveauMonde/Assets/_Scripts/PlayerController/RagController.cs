b61b09f7e44d7069772d9107ddfbf9bab56f2d4f
CH1M37 <CH1M37>
2018-11-19 19:14:26 +0100

    Ragdoll qui avance et recule en marchant

---
+++ NouveauMonde/Assets/_Scripts/PlayerController/RagController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagController : MonoBehaviour {
+
+    public GameObject CubeController;
+    public GameObject Animation;
+
+    public float speed = 0.1f;
+    
+	void Update () {
+
+        // Control by translation
+        /*
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }
+        */
+
+        // Control by animation
+        
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Animation.GetComponent<Animator>().enabled = true;
+            CubeController.transform.Translate(Vector3.forward * speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Animation.GetComponent<Animator>().enabled = true;
+            CubeController.transform.Translate(Vector3.back * speed);
+        }
+        else
+        {
+            Animation.GetComponent<Animator>().enabled = false;
+        }
+
+    }
+}
