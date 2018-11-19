234ba32131ea3cfb548407856c002df16d122467
CH1M37 <CH1M37>
2018-11-19 12:36:47 +0100

    Ragdoll qui marche

---
+++ NouveauMonde/Assets/_Scripts/PlayerController/MoveLegs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLegs : MonoBehaviour {
+
+    public HingeJoint hj;
+    public Transform objective;
+    public bool inverted;
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+        JointSpring js = hj.spring;
+
+        js.targetPosition = objective.localEulerAngles.x;
+
+        if (js.targetPosition > 180)
+        {
+            js.targetPosition = js.targetPosition - 360;
+        }
+
+        js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5); //Dans le if précédent ?
+
+        if (inverted)
+        {
+            js.targetPosition = js.targetPosition * -1;
+        }
+        hj.spring = js;
+
+	}
+}
